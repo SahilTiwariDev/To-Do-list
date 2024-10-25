8aae171edaea4a41332624a24522bb1b10b2b791
SahilTiwariDev <sahiltiwari.contactme@gmail.com>
2024-10-25 18:12:52 +0530

    Create basic structure and add color-codes for style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-green": "#E9F5DB",
+        "green-100": "#CFE1B9",
+        "green-200": "#B5C99A",
+        "green-300": "#97A97C",
+        "green-400": "#87986A",
+        "dark-green": "#718355",
+      },
+    },
   },
   plugins: [],
-}
-
+};
