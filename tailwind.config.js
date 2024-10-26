e18934f1d8c565c8bf1069e87dccf08caeccee3f
SahilTiwariDev <sahiltiwari.contactme@gmail.com>
2024-10-26 21:02:12 +0530

    Create basic structure of sign-up page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,6 @@ module.exports = {
         "moss-green": "#97A97C",
         "olive-green": "#87986A",
         "forest-green": "#718355",
-
-        // #E9F5DB → pale-green
-        // #CFE1B9 → soft-green
-        // #B5C99A → sage-green
-        // #97A97C → moss-green
-        // #87986A → olive-green
-        // #718355 → forest-green
       },
     },
   },
